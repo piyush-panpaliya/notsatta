1c7824272d63cb3af8bad5cb6b28ef0442fe0e78
piyush-panpaliya <panpaliyapiyush0@gmail.com>
2023-04-25 02:58:48 +0530

    fix: matchlistupdate api

--- src/pages/api/cron/updateMatchList.ts
+++ src/pages/api/cron/updateMatchList.ts
@@ -7,7 +7,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   try {
     await res.revalidate('/dash')
     const rooms = await prisma.room.findMany()
-    JSON.parse(req.body).forEach(async (match:getMatchType) => {
+    console.log(typeof req.body)
+    req.body.forEach(async (match:getMatchType) => {
       rooms.forEach(async (room) => {  ``
         await prisma.cmatch.create({
           data: {
