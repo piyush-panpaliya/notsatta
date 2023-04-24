9221c2a5ac87b52c8a66e5c3e7100d1fb7671c0a
piyush-panpaliya <panpaliyapiyush0@gmail.com>
2023-04-25 02:50:10 +0530

    fix:logging err to matchlistupdate api

--- src/pages/api/cron/updateMatchList.ts
+++ src/pages/api/cron/updateMatchList.ts
@@ -19,6 +19,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     }) 
     return res.json({ revalidated: true })
   } catch (err) {
+    console.log(err)
     return res.status(500).send('Error revalidating')
   }
 }
