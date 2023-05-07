0d9eeb147ac38bc537edc9092b608c30adb1c21b
piyush-panpaliya <panpaliyapiyush0@gmail.com>
2023-05-08 00:33:25 +0530

    fix:drawer,harcode old score, svg

--- cron/matchUpdate/src/index.ts
+++ cron/matchUpdate/src/index.ts
@@ -12,9 +12,9 @@ export default {
 		env: Env,
 		ctx: ExecutionContext
 	): Promise<void> {
-		const today = new Date(Date.now() + (5.5 * 60 * 60 * 1000))
-		console.log(today.toDateString())
-		const todayMatch = matches.filter(match => match.startTime.toDateString() === today.toDateString());
+		const today = new Date(Date.now() + (5 * 60 * 60 * 1000))
+		console.log(today.toLocaleDateString('en-US', { timeZone: "UTC" }))
+		const todayMatch = matches.filter(match => match.startTime.toLocaleDateString('en-US', { timeZone: "UTC" }) === today.toLocaleDateString('en-US', { timeZone: "UTC" }));
 		const fetchedMatch = await Promise.all(todayMatch.map(async (match) => await getMatch(match.link)))
 		console.log("adding this match to cmatch", todayMatch)
 		await fetch(env.API_URL, {
